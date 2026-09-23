e738b098bb51463f9ddf96be36be4af72f35cd26
mattchuahy <mattchuahy@gmail.com>
2026-09-23 14:57:08 +0800

    Update to test login function and login details pg

--- tests/testloginpage.test.ts
+++ tests/testloginpage.test.ts
@@ -1,14 +1,15 @@
 import { test, expect } from "@playwright/test";
 import { LoginPage } from "../pages/LoginPage";
 
-test("Navigate to login page", async ({ page }) => {
+test("Check Login page title", async ({ page }) => {
   const loginPage = new LoginPage(page);
 
   await loginPage.goto();
+
   await expect(page).toHaveTitle("Swag Labs");
 });
 
-test("Verify elements loaded", async ({ page }) => {
+test("Check elements loaded", async ({ page }) => {
   const loginPage = new LoginPage(page);
 
   await loginPage.goto();
